6fc67a6f67be5e0235ef84dfe6f6b4b9647fc0dd
agdsarge <agdsargeant@gmail.com>
2020-07-21 21:14:36 -0400

    channels and some methods in deal model

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -3,6 +3,19 @@ class Deal < ApplicationRecord
     belongs_to :game
     after_initialize :set_init_values
 
+    def hand
+        case self.position
+        when :north
+            return (0..12).map {|n| DECK[n*4] } + [DECK["reverse"]]
+        when :south
+            return (0..12).map {|n| DECK[n*4 + 1] } + [DECK["reverse"]]
+        when :east
+            return (0..12).map {|n| DECK[n*4 + 2]} + [DECK["reverse"]]
+        else
+            return return (0..12).map {|n| DECK[n*4 + 3] } + [DECK["reverse"]]
+        end
+    end
+
     private
     def shuffle
         self.deck_permutation = (0..51).to_a.shuffle.join(':')
